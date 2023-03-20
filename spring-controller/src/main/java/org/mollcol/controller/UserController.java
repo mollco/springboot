b6da9b68c3debf3aeab83d37a32bffebaf51a8a7
mollco <1216187725@qq.com>
2023-03-20 11:27:20 +0800

    修改

--- spring-controller/src/main/java/org/mollcol/controller/UserController.java
+++ spring-controller/src/main/java/org/mollcol/controller/UserController.java
@@ -5,7 +5,6 @@ import org.mollcol.service.userService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,14 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @Autowired
-    private userService userService ;
-    Logger logger= LoggerFactory.getLogger(this.getClass());
+    private userService userService;
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping("/view")
     @ResponseBody
-    public user test(@RequestParam String  user) {
-        logger.info("请求参数{}",user);
-      user user1 = userService.selectById(user);
-        System.out.println("haode");
-      return  user1;
+    public user test(@RequestParam String user) {
+        logger.info("请求参数{}", user);
+        user user1 = userService.selectById(user);
+        return user1;
     }
 }
